7e5eee9d1326fddf42050e3be9b702016df6dd3d
wfx <wolfgang.morawetz@gmail.com>
2015-02-02 22:58:27 +0100

    add dummy greeter callback's

--- enter.py
+++ enter.py
@@ -23,13 +23,27 @@ class Enter(ErigoGui):
         self.elm_entry_password.password = True
 
         self.greeter = LightDM.Greeter()
-
+        # connect signal handlers to LightDM
+        self.greeter.connect ("authentication-complete", self.authentication_complete_cb)
+        self.greeter.connect ("show-message", self.show_message_cb)
+        self.greeter.connect ("show-prompt", self.show_prompt_cb)
+        # connect to greeter
+        self.greeter.connect_sync()
 
 
     def elm_button_login_clicked_cb(self, btn):
         self._login_cb()
 
 
+    # This elm signal is called when the user hits enter after entering a
+    # username/password or clicks the login button.  Since we re-purposed
+    # the text entry box, we have 3 possible cases to handle here.
+    # 1) the user is already authenticated, if for example, they don't have
+    #  a password set.
+    # 2) The username has been passed into LightDM and now we need to pass
+    #  the password
+    # 3) The username has been entered, but not passed in.  We pass it in
+    #  and start the authentication process.
     def _login_cb(self):
         print (sys.stderr, "login_cb")
         if self.greeter.get_is_authenticated():
@@ -41,6 +55,44 @@ class Enter(ErigoGui):
         else:
             # Initial entry of username, send it to LightDM
             self.greeter.authenticate(self.elm_entry_username.entry_get())
+
+
+    # The show_prompt callback is oddly named, but when you get this
+    # callback you are supposed to send the password to LightDM next.  In
+    # our example, we re-purpose the prompt and ask the user for the
+    # password which is then sent the next time the user hits the Login
+    # button or presses enter.
+    def show_prompt_cb(greeter, text, promptType):
+        print(sys.stderr, text)
+
+        # clear the text entry box so the user can enter the password
+        print("clear the text entry box so the user can enter the password")
+
+        print(sys.stderr, "prompt type: " + str(promptType))
+        # if this is a password prompt, we want to hide the characters
+        if promptType == LightDM.PromptType.SECRET:
+            print(sys.stderr, "password promt")
+        else:
+            print(sys.stderr, "none password promt")
+
+
+    # If LightDM sends a message back to the greeter, for example, "Login
+    # failed" or "invalid password" we display it in our message box.
+    def show_message_cb(text, message_type):
+        print(sys.stderr, "get message: ", text)
+
+
+    # Callback for after we send LightDM the password, this method
+    # has to handle a successful login, in which case we start the session
+    # or a failed login, in which case we tell the user
+    def authentication_complete_cb(greeter):
+        if greeter.get_is_authenticated():
+            # For our simple example we always start Unity-2d.  The LightDM
+            # API has ways to query available sessions, please see the docs.
+            if not greeter.start_session_sync("enlightenment"):
+                print(sys.stderr, "Failed to start enlightenment")
+        else:
+            print(sys.stderr, "Login failed")
 
 
 if __name__ == '__main__':
