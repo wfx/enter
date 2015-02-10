2a992ceeadcce034f99463c786caee92e453d036
wfx <wolfgang.morawetz@gmail.com>
2015-02-10 16:17:04 +0100

    to use old/default way remove dev_test/ from .gitignore

--- dev_test.sh
+++ dev_test.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+if ! grep -r -n -i --include=.gitignore "./dev_test" 1> /dev/null
+then
+   echo "write into dev_test"
+   mkdir -p ./dev_test/{log,run,cache} 
+   /usr/sbin/lightdm --test-mode --debug --log-dir ./dev_test/log --run-dir ./dev_test/run --cache-dir ./dev_test/cache --config data/lightdm-efl-greeter.conf 
+else
+   /usr/sbin/lightdm --test-mode --debug --config data/lightdm-efl-greeter.conf
+fi
 
-/usr/sbin/lightdm --test-mode --debug --config data/lightdm-efl-greeter.conf
